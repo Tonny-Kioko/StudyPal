9acefbaef913d4cd90981e9aba762a425702668a
Anthony Kioko <102463336+Tonny-Kioko@users.noreply.github.com>
2022-07-28 23:39:26 +0300

    Static root

--- studypal/settings.py
+++ studypal/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = '/staticfiles/'
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
