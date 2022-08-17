2120c6d1a09c565f5f56f4f8f218ae1a945f4d26
Tonny-Kioko <anthonykioko5525@gmail.com>
2022-08-17 17:03:29 +0300

    cheer up

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -6,6 +6,7 @@ urlpatterns = {
     path('login/', views.loginPage, name="login"), 
     path('logout/', views.logoutPage, name="logout"),
     path('register/', views.registerUser, name="register"),
+    path('register/', views.registerUser, name="register"),
 
 
 }
